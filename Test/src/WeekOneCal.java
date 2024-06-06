1abdfa3e9c4b41de5e3b677fd6a347bafc26ce77
Kaviru Hapuarachchi <hapuarachchikaviru@gmail.com>
2024-06-06 09:32:07 +0530

    WeekOneCal

--- Test/src/WeekOneCal.java
+++ Test/src/WeekOneCal.java
@@ -1,4 +1,18 @@
 public class WeekOneCal {
 
-    
+    public static void main(String[] args) {
+        int myNum = 5;
+        float myScore = 5.99f;
+        char myChar = 'A';
+        boolean myBool = true;
+        String myString = "Hello World";
+
+        System.out.println(myNum);
+        System.out.println(myScore);
+        System.out.println(myChar);
+        System.out.println(myBool);
+        System.out.println(myString);
+
+    }
+
 }
---
+++ Test/src/WeekOneTwo.java
@@ -0,0 +1,9 @@
+public class WeekOneTwo {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
