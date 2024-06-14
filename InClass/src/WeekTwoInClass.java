dd0d8a5ae72a131ad9d7127fe071683e1d99a2cc
kaviru <hapuarachchikaviru@gmail.com>
2024-06-14 10:31:38 +0530

    SubsString

--- InClass/src/WeekTwoInClass.java
+++ InClass/src/WeekTwoInClass.java
@@ -2,8 +2,12 @@ public class WeekTwoInClass {
 
     public static void main(String[] args) {
 
-        String myString = "Hello World";
-        System.out.println(myString);
+        String str1 = "Hello";
+        String str2 = "Name";
+        String str3 = "Welcome"+" " + str2;
 
+        String newString = str3.substring(0,9);
+
+        System.out.println(newString);
     }
 }
