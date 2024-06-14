2b741c3c54d83a1dfd31d90511693ff072b45d0d
kaviru <hapuarachchikaviru@gmail.com>
2024-06-14 10:43:37 +0530

    Compare

--- InClass/src/WeekTwoInClass.java
+++ InClass/src/WeekTwoInClass.java
@@ -4,11 +4,16 @@ public class WeekTwoInClass {
 
         String str1 = "Hello";
         String str2 = "Name";
+        String str4 = "Name";
         String str3 = "Welcome"+" " + str2;
+        int num1 = 10;
+        int num2 = 12;
 
         String newString = str3.substring(0,9);
 
-        System.out.println(newString);
-        System.out.println(str1.toUpperCase());
+//        System.out.println(newString);
+//        System.out.println(str1.toUpperCase());
+//        System.out.println(num1===num2);
+
     }
 }
