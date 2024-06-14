15074c709fa5fc0dbc52adc072b36d02d478e278
kaviru <hapuarachchikaviru@gmail.com>
2024-06-14 10:23:18 +0530

    Inclass

--- Test/src/WeekOneTwo.java
+++
@@ -1,9 +0,0 @@
-public class WeekOneTwo {
-
-    public static void main(String[] args) {
-
-        
-
-    }
-
-}
--- Test/src/WeekTwoInClass.java
+++
@@ -1,9 +0,0 @@
-public class WeekTwoInClass {
-
-    public static void main(String[] args) {
-
-        String myString = "Hello World";
-        System.out.println(myString);
-
-    }
-}
